a5648576a547580c10bc6df6e19ad6aa4714c4c0
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-24 16:51:42 -0700

    init components, filetree

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Main from "./views/Main/Main";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+    <Header />
+    <Main />
+    <Footer />
+    </>
+  );
 }
