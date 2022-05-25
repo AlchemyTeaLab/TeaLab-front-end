476fd92dfc9b807e204b66c1fb7bfec132c93460
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-25 10:45:29 -0700

    wrote signup/in, signout services

--- src/services/users.js
+++ src/services/users.js
@@ -1,11 +1,44 @@
 export const signIn = ({ email, password }) => {
   // fetch post
+  try {
+    const res = await fetch(`${process.env.API_URL}/api/v1/users/session`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+      mode: 'cors',
+      body: JSON.stringify({ email, password })
+    });
+    return res.json();
+
+  } catch (error) {
+    throw new Error('Invalid credentials');
+  }
 };
 
 export const signUp = ({ email, password, username }) => {
   // fetch post
+  try {
+    const res = await fetch(`${process.env.API_URL}/api/v1/users`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+      mode: 'cors',
+      body: JSON.stringify({ username, email, password })
+    });
+    return res.json();
+    
+  } catch (error) {
+    throw new Error('Invalid credentials');
+  }
 };
 
 export const signOut = () => {
   // fetch delete
+  const res = await fetch(`${process.env.API_URL}/api/v1/users`, {
+    method: 'DELETE',
+    credentials: 'include',
+    mode: 'cors'
+  });
+
+  return res.ok;
 };
