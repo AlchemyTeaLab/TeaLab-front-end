5edf08cf8cc09519a5d09461038f8aeeca103f5f
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-05-25 12:16:36 -0700

    set up mock server in test setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,17 @@
 import fetch from 'cross-fetch';
 global.fetch = fetch;
+
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { mockSignUpData, mockSignInData, mockUserData } from './fixtures/mockAuthData';
+
+const server = setupServer(
+    rest.post(`${process.env.API_URL}/api/v1/users`, (req, res, ctx) => res(ctx.json(mockSignUpData)) ),
+    rest.post(`${process.env.API_URL}/api/v1/users/session`, (req, res, ctx) => res(ctx.json(mockSignInData))),
+    rest.get(`${process.env.API_URL}/api/v1/users/me`, (req, res,ctx) => ctx.json(mockUserData))
+
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
