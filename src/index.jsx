72398dd03e7c813f225e85bf4129e42b280eb85a
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-25 16:07:16 -0700

    TeaProvider setup, state init for ingredients.

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,15 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import AuthProvider from './context/AuthProvider';
+import TeaProvider from './context/TeaProvider';
 
 render(
   <React.StrictMode>
     <Router>
       <AuthProvider>
-        <App />
+        <TeaProvider>
+          <App />
+        </TeaProvider>
       </AuthProvider>
     </Router>
   </React.StrictMode>,
