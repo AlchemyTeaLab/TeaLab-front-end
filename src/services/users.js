9866417f5f1b35ca0f20d99e2e0fb374754a8af1
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-24 17:54:13 -0700

    init AuthProvider, useAuth hook, init users service functions signIn, signUp, signOut.

---
+++ src/services/users.js
@@ -0,0 +1,11 @@
+export const signIn = ({ email, password }) => {
+  // fetch post
+};
+
+export const signUp = ({ email, password, username }) => {
+  // fetch post
+};
+
+export const signOut = () => {
+  // fetch delete
+};
