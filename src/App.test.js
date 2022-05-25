79c9f96ed4b7f7ba5b34730965730e4d62b8a7e3
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-05-25 15:30:57 -0700

    wrote test for sign up

--- src/App.test.js
+++ src/App.test.js
@@ -25,6 +25,37 @@ describe('<App />', () => {
 
     
     })
+
+    it('sign up a user', async () => {
+        render(
+            <MemoryRouter initialEntries={['/login']} initialIndex={0}>
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            </MemoryRouter>
+        );
+
+        const toggleSignIn = screen.getByLabelText(
+            /toggle sign in for existing users/i
+          );
+          const toggleSignUp = screen.getByLabelText(/toggle sign up for new users/i);
+
+          userEvent.click(toggleSignUp);
+
+          const usernameInput = screen.getByPlaceholderText(/username/i);
+          userEvent.type(usernameInput, 'test user');
+
+          const emailInput = screen.getByPlaceholderText(/email/i);
+        userEvent.type(emailInput, 'test@test.com');
+
+        const passInput = screen.getByPlaceholderText(/password/i);
+        userEvent.type(passInput, '123456');
+
+        const signUpButton = screen.getByRole('button', { name: /sign up/i});
+        userEvent.click(signUpButton);
+
+
+    })
     
 })
 
