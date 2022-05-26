c6113dff0eb953edcac8f72a0f63f22f8768b612
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-26 16:31:03 -0700

    signOut works, still need auto refresh

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useHistory} from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
 
 export default function Footer() {
+  const { user, setUser, signOutUser } = useAuth();
+  const history = useHistory();
+
+  async function handleSignOut() {
+    await signOutUser();
+    setUser(null);
+    history.replace('/login');
+  }
+
   return (
-    <footer>Footer</footer>
+    <footer>
+      <button onClick={handleSignOut}>Sign Out</button>
+    </footer>
   )
 }
