f199e783eaf09c4077643b69a3d162fd6c58c2c6
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-26 17:13:40 -0700

    edited useAuth hooks path

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { Redirect, Route } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
+import { useAuth } from '../../hooks/useAuth';
 
 export default function PrivateRoute({ children, ...rest }) {
   const { user } = useAuth();
