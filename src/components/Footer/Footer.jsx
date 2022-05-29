bb7096c20eb4066143c31dd6f4881950947910bd
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-29 14:02:57 -0700

    update footer nav routes for location.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,10 @@
 import { useHistory, useLocation, Link } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
-import Loading from '../Loading/Loading';
 
 export default function Footer() {
   const { user, setUser, signOutUser } = useAuth();
   const history = useHistory();
-  const location = useLocation();
+  const { pathname } = useLocation();
   const routes = [
     { path: '/ingredients', name: 'Home' },
     { path: '/about', name: 'About' },
@@ -28,11 +27,14 @@ export default function Footer() {
       <footer>
         <nav>
           <ul>
-            {routes.map((route, i) => (
-              <li key={`${route.name}${i}`}>
-                <Link to={route.path}>{route.name}</Link>
-              </li>
-            ))}
+            {routes.map(
+              ({ name, path }, i) =>
+                pathname !== path && (
+                  <li key={`${name}${i}`}>
+                    <Link to={path}>{name}</Link>
+                  </li>
+                )
+            )}
             <li>{signInOutButton}</li>
           </ul>
         </nav>
