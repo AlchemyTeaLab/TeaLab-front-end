9d6537b84fa64d68005a655c2a5177d87540ae8a
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-29 13:17:14 -0700

    form styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Footer from './components/Footer/Footer';
 import Main from './views/Main/Main';
 import Header from './components/Header/Header';
-import { useAuth } from './hooks/useAuth';
 import Loading from './components/Loading/Loading';
+import './App.css';
+import { useAuth } from './hooks/useAuth';
 
 export default function App() {
   const { loading } = useAuth();
--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
-import styles from '../../App.css';
+import styles from './Auth.css';
 import { useAuth } from '../../hooks/useAuth';
 
 export default function Auth() {
