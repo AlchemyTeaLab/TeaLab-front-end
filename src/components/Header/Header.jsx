d4b70f9ff0fc7da8d064561fa445365b1440014d
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-29 11:24:29 -0700

    init component Header elements.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { useAuth } from '../../hooks/useAuth';
 
 export default function Header() {
+  const { username } = useAuth();
+
   return (
-    <header>Header</header>
-  )
+    <>
+      <header>
+        <h1>
+          {'<'}TeaLab{' />'}
+        </h1>
+        <p>
+          Welcome <span>{user.username}</span>!
+        </p>
+      </header>
+    </>
+  );
 }
