a414ae05ce720cd27b7bd6082886cdfb4b56a7f1
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-29 11:58:43 -0700

    update component Header elements for user ?:, init component Footer elements, nav.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,21 @@
-import { useHistory} from 'react-router-dom';
+import { useHistory, useLocation, Link } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
+import Loading from '../Loading/Loading';
 
 export default function Footer() {
   const { user, setUser, signOutUser } = useAuth();
   const history = useHistory();
+  const location = useLocation();
+  const routes = [
+    { path: '/ingredients', name: 'Home' },
+    { path: '/about', name: 'About' },
+  ];
+
+  const signInOutButton = user.email ? (
+    <button onClick={handleSignOut}>Sign Out</button>
+  ) : (
+    <button onClick={() => history.push('/login')}>Sign In</button>
+  );
 
   async function handleSignOut() {
     await signOutUser();
@@ -12,8 +24,19 @@ export default function Footer() {
   }
 
   return (
-    <footer>
-      <button onClick={handleSignOut}>Sign Out</button>
-    </footer>
-  )
+    <>
+      <footer>
+        <nav>
+          <ul>
+            {routes.map((route, i) => (
+              <li key={`${route.name}${i}`}>
+                <Link to={route.path}>{route.name}</Link>
+              </li>
+            ))}
+            <li>{signInOutButton}</li>
+          </ul>
+        </nav>
+      </footer>
+    </>
+  );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 import { useAuth } from '../../hooks/useAuth';
 
 export default function Header() {
-  const { username } = useAuth();
+  const { user } = useAuth();
 
   return (
     <>
@@ -12,9 +10,11 @@ export default function Header() {
         <h1>
           {'<'}TeaLab{' />'}
         </h1>
-        <p>
-          Welcome <span>{user.username}</span>!
-        </p>
+        {user.email && (
+          <p>
+            Welcome <span>{user.username}</span>!
+          </p>
+        )}
       </header>
     </>
   );
