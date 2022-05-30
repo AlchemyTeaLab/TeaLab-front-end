508ced4452ebbf80ecd401edb9f0ce7eec603b66
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-30 10:56:52 -0700

    render component Header el for pathname.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 import styles from './Header.css';
 
 export default function Header() {
   const { user } = useAuth();
+  const { pathname } = useLocation();
+  const publicLocation = ['/', '/login', '/about'].includes(pathname);
 
   return (
     <>
-      <header>
+      <header className={publicLocation ? styles.public : null}>
         <h1>TeaLab</h1>
-        {user.email && (
+        {user.email && !publicLocation && (
           <p>
             Welcome <span className={styles.username}>{user.username}</span>!
           </p>
