fdc9348483c6375056e4fa3d56ac1ef7fb814a59
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-05-31 09:17:29 -0700

    initial ingredient detail written

--- src/components/IngredientItem/IngredientItem.jsx
+++ src/components/IngredientItem/IngredientItem.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
-export default function IngredientItem({ ingredient, handleChange }) {
+export default function IngredientItem({ ingredient }) {
+  const { id, commonName, scientificName, type, healthBenefits, description } = ingredient;
+
+  function handleDetail() {
+    //Pop up Detail
+  };
   return (
     <>
-    <input type='checkbox' value={ingredient.id} name={ingredient.commonName} onChange={handleChange} />
+    <input type='checkbox' value={ingredient.commonName} name={ingredient.commonName}/>
     <label htmlFor={ingredient.commonName}>{ingredient.commonName}</label>
-    <button>❔</button>
+    <button onClick={handleDetail}>❔</button>
     </>
   )
 }
