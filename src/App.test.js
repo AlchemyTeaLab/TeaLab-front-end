754067d6d4f17d1e3e011e984dfc028b21979dc3
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-05-31 12:40:47 -0700

    tests for App pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import AuthProvider from './context/AuthProvider';
@@ -17,13 +17,22 @@ describe('<App />', () => {
             </MemoryRouter>
         );
 
+        const loading = screen.getByText(/loading/i)
+        await waitForElementToBeRemoved(loading);
+
         const emailInput = screen.getByRole('textbox');
         userEvent.type(emailInput, 'test@test.com');
 
         const passInput = screen.getByPlaceholderText(/password/i);
         userEvent.type(passInput, '123456');
+        const form = screen.getByRole('form', {
+            name: /sign in or sign up to continue/i
+          });
+        
+        const signInButton = within(form).getByRole('button', {
+            name: /sign in/i
+          });
 
-        const signInButton = screen.getByRole('button', { name: /sign in/i });
         userEvent.click(signInButton);
     });
 
@@ -37,24 +46,27 @@ describe('<App />', () => {
                 </AuthProvider>
             </MemoryRouter>
         );
+        const loading = screen.getByText(/Loading/i)
+    
+        await waitForElementToBeRemoved(loading);
 
         const toggleSignIn = screen.getByLabelText(/toggle sign in for existing users/i);
-
+        
         const toggleSignUp = screen.getByLabelText(/toggle sign up for new users/i);
-
         userEvent.click(toggleSignUp);
-
+        
         const usernameInput = screen.getByPlaceholderText(/username/i);
         userEvent.type(usernameInput, 'test user');
-
+        
         const emailInput = screen.getByPlaceholderText(/email/i);
         userEvent.type(emailInput, 'test@test.com');
-
+        
         const passInput = screen.getByPlaceholderText(/password/i);
         userEvent.type(passInput, '123456');
-
+        
         const signUpButton = screen.getByRole('button', { name: /sign up/i });
         userEvent.click(signUpButton);
+        screen.debug();
     });  
 });
 
