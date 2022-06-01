79a32049f2d906586110585ff70dd6bdf2de4fe5
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-31 17:51:21 -0700

    update Profile for recipes.

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -8,7 +8,7 @@ export default function Profile() {
 
   useEffect(() => {
     const getData = async () => {
-      await getUserRecipes(user.id);
+      const recipes = await getUserRecipes(user.id);
     };
     getData();
   }, []);
