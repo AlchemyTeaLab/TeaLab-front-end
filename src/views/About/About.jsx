5236d02bfe77c781f805a609a0ab9060e98fc1e8
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-06-01 08:36:02 -0700

    wrote initial about page

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -2,6 +2,48 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+    <h2>About</h2>
+    <div>
+      <h3>Sam Melius Oxford</h3>
+      <p>Bio: Full-stack software developer based out of Mesa, Az.</p>
+      <p>Favorite Tea: Arnold Palmer...</p>
+      <p>Github: https://github.com/Sam-Melius</p>
+      <p>LinkedIn: https://www.linkedin.com/in/sam-melius-oxford/</p>
+      <p>Email: samsjmelius@gmail.com</p>
+    </div>
+    <div>
+      <h3>Clare McDonald</h3>
+      <p>Bio: </p>
+      <p>Favorite Tea: The blood of my enemies!</p>
+      <p>Github: </p>
+      <p>LinkedIn: </p>
+      <p>Email: </p>
+    </div>
+    <div>
+      <h3>Ari Harlem-Caballero</h3>
+      <p>Bio: </p>
+      <p>Favorite Tea: Sam's tears...</p>
+      <p>Github: </p>
+      <p>LinkedIn: </p>
+      <p>Email: </p>
+    </div>
+    <div>
+      <h3>Alice Hsing</h3>
+      <p>Bio: </p>
+      <p>Favorite Tea: The hopes and dreams of children</p>
+      <p>Github: </p>
+      <p>LinkedIn: </p>
+      <p>Email: </p>
+    </div>
+    <div>
+      <h3>Ryan Flitcroft</h3>
+      <p>Bio: </p>
+      <p>Favorite Tea: Just Tea</p>
+      <p>Github: </p>
+      <p>LinkedIn: </p>
+      <p>Email: </p>
+    </div>
+    </>
   )
 }
