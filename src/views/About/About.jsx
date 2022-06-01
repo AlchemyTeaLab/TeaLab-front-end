c5f0d0f46555354503c1f16fd27a647ec73a53a8
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-06-01 08:58:06 -0700

    added contact links to about page

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -3,46 +3,51 @@ import React from 'react'
 export default function About() {
   return (
     <>
-    <h2>About</h2>
+    <h2>About the Developers</h2>
     <div>
       <h3>Sam Melius Oxford</h3>
       <p>Bio: Full-stack software developer based out of Mesa, Az.</p>
       <p>Favorite Tea: Arnold Palmer...</p>
-      <p>Github: https://github.com/Sam-Melius</p>
-      <p>LinkedIn: https://www.linkedin.com/in/sam-melius-oxford/</p>
-      <p>Email: samsjmelius@gmail.com</p>
+      <p>Contact Info:</p>
+      <a href='https://github.com/Sam-Melius'>https://github.com/Sam-Melius</a><br/>
+      <a href='https://www.linkedin.com/in/sam-melius-oxford/'>https://www.linkedin.com/in/sam-melius-oxford/</a><br/>
+      <a href='samsjmelius@gmail.com'>samsjmelius@gmail.com</a>
     </div>
     <div>
       <h3>Clare McDonald</h3>
       <p>Bio: </p>
       <p>Favorite Tea: The blood of my enemies!</p>
-      <p>Github: </p>
-      <p>LinkedIn: </p>
-      <p>Email: </p>
+      <p>Contact Info:</p>
+      <a href=''>Github: </a><br/>
+      <a href=''>LinkedIn: </a><br/>
+      <a href=''>Email: </a>
     </div>
     <div>
       <h3>Ari Harlem-Caballero</h3>
       <p>Bio: </p>
       <p>Favorite Tea: Sam's tears...</p>
-      <p>Github: </p>
-      <p>LinkedIn: </p>
-      <p>Email: </p>
+      <p>Contact Info:</p>
+      <a href=''>Github: </a><br/>
+      <a href=''>LinkedIn: </a><br/>
+      <a href=''>Email: </a>
     </div>
     <div>
       <h3>Alice Hsing</h3>
       <p>Bio: </p>
       <p>Favorite Tea: The hopes and dreams of children</p>
-      <p>Github: </p>
-      <p>LinkedIn: </p>
-      <p>Email: </p>
+      <p>Contact Info:</p>
+      <a href=''>Github: </a><br/>
+      <a href=''>LinkedIn: </a><br/>
+      <a href=''>Email: </a>
     </div>
     <div>
       <h3>Ryan Flitcroft</h3>
       <p>Bio: </p>
       <p>Favorite Tea: Just Tea</p>
-      <p>Github: </p>
-      <p>LinkedIn: </p>
-      <p>Email: </p>
+      <p>Contact Info:</p>
+      <a href=''>Github: </a><br/>
+      <a href=''>LinkedIn: </a><br/>
+      <a href=''>Email: </a>
     </div>
     </>
   )
