e6610468a8e73377d4ac698ff32da49e46a40b5f
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-06-01 11:32:39 -0700

    RecipeItem, Profile from alice

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,17 +1,28 @@
 import React, { useEffect } from 'react';
+import RecipeItem from '../../components/RecipeItem/RecipeItem';
 import { useAuth } from '../../hooks/useAuth';
 import { useRecipes } from '../../hooks/useRecipes';
-
 export default function Profile() {
   const { user } = useAuth();
   const { recipes, getUserRecipes } = useRecipes();
-
   useEffect(() => {
     const getData = async () => {
       const recipes = await getUserRecipes(user.id);
     };
     getData();
   }, []);
-
-  return <div>Profile</div>;
+  return (
+    <>
+      <div>Profile</div>
+      <section>
+        <ul>
+          {recipes.map((recipe, i) => (
+            <li key={`${recipe.id}-${i}`}>
+              <RecipeItem recipe={recipe} />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
 }
