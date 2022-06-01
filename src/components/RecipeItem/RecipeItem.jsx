7f9020fb62459574c290725427dc11149f1c7f82
Alice Hsing <alicehsing@gmail.com>
2022-06-01 11:13:14 -0700

    added profile content

--- src/components/RecipeItem/RecipeItem.jsx
+++ src/components/RecipeItem/RecipeItem.jsx
@@ -1,13 +1,45 @@
 import { useState } from 'react';
+import styles from './RecipeItem.css';
 
-export default function RecipeItem({ recipe }) {
+export default function RecipeItem({ recipe, updateRecipe, removeRecipe }) {
   const [isEditing, setIsEditing] = useState(false);
 
   let content;
   if (!isEditing) {
-    content = <></>;
+    content = (
+      <>
+        <div className={styles.buttons}>
+          <button type="button" onClick={() => setIsEditing(true)}>
+            Edit
+          </button>
+          <button type="button" onClick={() => setIsEditing(true)}>
+            Delete
+          </button>
+        </div>
+      </>
+    );
   } else {
-    content = <></>;
+    console.log('RECIPE', recipe);
+    content = (
+      <>
+        <form>
+          <h3>{recipe.name}</h3>
+          <input
+            value={recipe.notes}
+            aria-label="Edit field"
+            onChange={(e) => {
+              updateRecipe({
+                ...recipe,
+                notes: e.target.value,
+              });
+            }}
+          />
+          <button type="submit" aria-label="Save changes">
+            Save
+          </button>
+        </form>
+      </>
+    );
   }
   return content;
 }
