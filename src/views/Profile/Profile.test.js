bd74100b663d281294b3318a5b66c96e3662d4e5
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-06-02 11:47:16 -0700

    wrote test for profile view. test is passing.

--- src/views/Profile/Profile.test.js
+++ src/views/Profile/Profile.test.js
@@ -35,6 +35,61 @@ describe('<App />', () => {
             const home = screen.getByText('Base');
             expect(home).toBeInTheDocument();
 
+            const checkbox = screen.getByRole('checkbox', {
+                name: /add oolong tea to your recipe!/i
+              });
+            userEvent.click(checkbox);
+
+            const addBase = screen.getByRole('button', {
+                name: /select a base to continue/i
+              });
+            userEvent.click(addBase);
+
+            const flavor = screen.getByText('Flavor');
+            expect(flavor).toBeInTheDocument();
+
+            const checkbox2 = screen.getByRole('checkbox', {
+                name: /add lemon to your recipe!/i
+              });
+            userEvent.click(checkbox2);
+
+            const addFlavor = screen.getByRole('button', {
+                name: /select a flavor to continue/i
+              });
+            userEvent.click(addFlavor);
+
+            const boost = screen.getByText('Boost');
+            expect(boost).toBeInTheDocument();
+
+            const checkbox3 = screen.getByRole('checkbox', {
+                name: /add protein to your recipe!/i
+              });
+            userEvent.click(checkbox3);
+
+            const addBoost = screen.getByRole('button', {
+                name: /brew you tea!/i
+              });
+            userEvent.click(addBoost);
+
+            const profile = screen.getByText('Profile');
+            expect(profile).toBeInTheDocument();
+
+            const edit = screen.getByRole('button', {
+                name: /edit/i
+              });
+            userEvent.click(edit);
+
+            const note = screen.getByRole('textbox', {
+                name: /edit field/i
+              })
+            userEvent.type('test note');
+            const save = screen.getByRole('button', {
+                name: /save changes/i
+              });
+            userEvent.click(save);
+
+            const testNote = screen.getByText(/test note/i);
+            expect(testNote).toBeInTheDocument();
             
         });
 
