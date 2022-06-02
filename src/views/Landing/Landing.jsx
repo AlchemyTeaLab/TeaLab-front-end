422cca6283d9566cac5ab93984c987b7f4ef3295
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-06-02 10:56:34 -0700

    updated styles

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { useTour } from '@reactour/tour';
+import styles from './Landing.css';
 
 export default function Landing() {
   const { setIsOpen } = useTour();
 
   return (
-    <div>
-      <button onClick={() => setIsOpen(true)}>
-        Take a tour of TeaLab with ? as your guide!</button>
-    </div>
-  )
+    <>
+      <div>
+        <button onClick={() => setIsOpen(true)}>
+          Take a tour of TeaLab with ? as your guide!
+        </button>
+      </div>
+      <img
+        className={styles.ruby}
+        src="https://res.cloudinary.com/tealab/image/upload/v1653970382/vector_assets/ruby_trans.svg"
+        alt=""
+      />
+    </>
+  );
 }
