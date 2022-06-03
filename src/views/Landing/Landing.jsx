7557944707ed0e3d1fde48ad479176b2f705e1f7
Alice Hsing <alicehsing@gmail.com>
2022-06-03 09:15:23 -0700

    cleaned up unused code

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { useTour } from '@reactour/tour';
 import styles from './Landing.css';
-import { useHistory } from 'react-router-dom';
 
 export default function Landing() {
   const { setIsOpen } = useTour();
-  const history = useHistory();
 
   return (
     <>
       <div>
         <button onClick={() => setIsOpen(true)}>
-        Take a tour of TeaLab with Ruby the Tea Sommelier as your guide!
+          Take a tour of TeaLab with Ruby the Tea Sommelier as your guide!
         </button>
       </div>
       <img
